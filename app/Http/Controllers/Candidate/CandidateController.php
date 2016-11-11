1f14d3598ad7698a14be026c613775656ca9b00c
Vimal <vimal@techvistara.com>
2016-11-11 11:06:58 +0530

    First Commit By Vimal

--- app/Http/Controllers/Candidate/CandidateController.php
+++ app/Http/Controllers/Candidate/CandidateController.php
@@ -913,4 +913,224 @@ class CandidateController extends Controller
 
     }
 
+
+    /* Get list of getCandidatesByQuickSearch
+     * @params none
+     * @throws $candidateException
+     * @return array | null
+     * @author Baskar
+     */
+
+    public function getCandidatesByQuickSearch(Request $candidateRequest)
+    {
+        $candidates = null;
+        $responseJson = null;
+        //dd('Inside candidates list controller');
+
+        try
+        {
+            $paginate = $candidateRequest->get('paginate');
+            $candidates = $this->candidateService->getCandidates($paginate);
+            //dd($candidates);
+            if(!empty($candidates))
+            {
+                $responseJson = new ResponseJson(ErrorEnum::SUCCESS, trans('messages.'.ErrorEnum::CANDIDATE_LIST_SUCCESS));
+                //$responseJson->setObj($candidates);
+                //$responseJson->sendSuccessResponse();
+            }
+            else
+            {
+                //dd('Inside else statement');
+                $responseJson = new ResponseJson(ErrorEnum::SUCCESS, trans('messages.'.ErrorEnum::NO_CANDIDATE_LIST_FOUND));
+                //$responseJson->setObj($candidates);
+                //$responseJson->sendSuccessResponse();
+            }
+
+            $responseJson->setObj($candidates);
+            $responseJson->sendSuccessResponse();
+        }
+        catch(CandidateException $candidateExc)
+        {
+            //dd($candidateExc);
+
+            /*$errorMsg = $candidateExc->getMessageForCode();
+            $msg = AppendMessage::appendMessage($candidateExc);
+            Log::error($msg);*/
+
+            $responseJson = new ResponseJson(ErrorEnum::FAILURE, trans('messages.'.ErrorEnum::CANDIDATE_LIST_ERROR));
+            $responseJson->sendErrorResponse($candidateExc);
+        }
+        catch(Exception $exc)
+        {
+            //dd($exc);
+            $msg = AppendMessage::appendGeneralException($exc);
+            Log::error($msg);
+        }
+
+        return $responseJson;
+    }
+
+
+    /* Get list of getCandidatesByBasicSearch
+     * @params none
+     * @throws $candidateException
+     * @return array | null
+     * @author Baskar
+     */
+
+    public function getCandidatesByBasicSearch(Request $candidateRequest)
+    {
+        $candidates = null;
+        $responseJson = null;
+        //dd('Inside candidates list controller');
+
+        try
+        {
+            $paginate = $candidateRequest->get('paginate');
+            $candidates = $this->candidateService->getCandidates($paginate);
+            //dd($candidates);
+            if(!empty($candidates))
+            {
+                $responseJson = new ResponseJson(ErrorEnum::SUCCESS, trans('messages.'.ErrorEnum::CANDIDATE_LIST_SUCCESS));
+                //$responseJson->setObj($candidates);
+                //$responseJson->sendSuccessResponse();
+            }
+            else
+            {
+                //dd('Inside else statement');
+                $responseJson = new ResponseJson(ErrorEnum::SUCCESS, trans('messages.'.ErrorEnum::NO_CANDIDATE_LIST_FOUND));
+                //$responseJson->setObj($candidates);
+                //$responseJson->sendSuccessResponse();
+            }
+
+            $responseJson->setObj($candidates);
+            $responseJson->sendSuccessResponse();
+        }
+        catch(CandidateException $candidateExc)
+        {
+            //dd($candidateExc);
+
+            /*$errorMsg = $candidateExc->getMessageForCode();
+            $msg = AppendMessage::appendMessage($candidateExc);
+            Log::error($msg);*/
+
+            $responseJson = new ResponseJson(ErrorEnum::FAILURE, trans('messages.'.ErrorEnum::CANDIDATE_LIST_ERROR));
+            $responseJson->sendErrorResponse($candidateExc);
+        }
+        catch(Exception $exc)
+        {
+            //dd($exc);
+            $msg = AppendMessage::appendGeneralException($exc);
+            Log::error($msg);
+        }
+
+        return $responseJson;
+    }
+
+
+    /* Get list of getCandidatesByAdvanceSearch
+     * @params none
+     * @throws $candidateException
+     * @return array | null
+     * @author Baskar
+     */
+
+    public function getCandidatesByAdvanceSearch(Request $candidateRequest)
+    {
+        $candidates = null;
+        $responseJson = null;
+        //dd('Inside candidates list controller');
+
+        try
+        {
+            $paginate = $candidateRequest->get('paginate');
+            $candidates = $this->candidateService->getCandidates($paginate);
+            //dd($candidates);
+            if(!empty($candidates))
+            {
+                $responseJson = new ResponseJson(ErrorEnum::SUCCESS, trans('messages.'.ErrorEnum::CANDIDATE_LIST_SUCCESS));
+                //$responseJson->setObj($candidates);
+                //$responseJson->sendSuccessResponse();
+            }
+            else
+            {
+                //dd('Inside else statement');
+                $responseJson = new ResponseJson(ErrorEnum::SUCCESS, trans('messages.'.ErrorEnum::NO_CANDIDATE_LIST_FOUND));
+                //$responseJson->setObj($candidates);
+                //$responseJson->sendSuccessResponse();
+            }
+
+            $responseJson->setObj($candidates);
+            $responseJson->sendSuccessResponse();
+        }
+        catch(CandidateException $candidateExc)
+        {
+            //dd($candidateExc);
+
+            /*$errorMsg = $candidateExc->getMessageForCode();
+            $msg = AppendMessage::appendMessage($candidateExc);
+            Log::error($msg);*/
+
+            $responseJson = new ResponseJson(ErrorEnum::FAILURE, trans('messages.'.ErrorEnum::CANDIDATE_LIST_ERROR));
+            $responseJson->sendErrorResponse($candidateExc);
+        }
+        catch(Exception $exc)
+        {
+            //dd($exc);
+            $msg = AppendMessage::appendGeneralException($exc);
+            Log::error($msg);
+        }
+
+        return $responseJson;
+    }
+
+
+    /* Get candidate details
+     * @params $companyId
+     * @throws $companyExc
+     * @return array | null
+     * @author Baskar
+     */
+
+    public function getCandidateDetailsBySearch($candidateId)
+    {
+        $candidateDetails = null;
+        $responseJson = null;
+        //dd('Inside candidate details controller');
+
+        try
+        {
+            $candidateDetails = $this->candidateService->getCandidateDetails($candidateId);
+            //dd($listGroups);
+            if(!empty($candidateDetails))
+            {
+                $responseJson = new ResponseJson(ErrorEnum::SUCCESS, trans('messages.'.ErrorEnum::CANDIDATE_DETAILS_SUCCESS));
+
+            }
+            else
+            {
+                $responseJson = new ResponseJson(ErrorEnum::SUCCESS, trans('messages.'.ErrorEnum::NO_CANDIDATE_DETAILS_FOUND));
+            }
+
+            $responseJson->setObj($candidateDetails);
+            $responseJson->sendSuccessResponse();
+        }
+        catch(CandidateException $candidateExc)
+        {
+            //dd($helperExc);
+            $responseJson = new ResponseJson(ErrorEnum::FAILURE, trans('messages.'.ErrorEnum::CANDIDATE_DETAILS_ERROR));
+            $responseJson->sendErrorResponse($candidateExc);
+
+        }
+        catch(Exception $exc)
+        {
+            //dd($exc);
+            $msg = AppendMessage::appendGeneralException($exc);
+            Log::error($msg);
+        }
+
+        return $responseJson;
+    }
+
+
 }
